0787d061b13e7b96ff0773965a14fc668735d013
David D'Agostino <dagostin@symplicity.com>
2017-03-26 14:57:21 -0400

    cleanup skill

--- apps/tuner/index.js
+++ apps/tuner/index.js
@@ -9,37 +9,48 @@ app.launch(function(req, res) {
     console.log("launch skill");
     res.say(prompt).reprompt(prompt).shouldEndSession(false);
 });
-
+           
 app.intent('TuneIntent', '',
  function (req, res) {
      var spokenNote;
      var note;
 
-     //add {sharp|flat} optional ending
-     //find wave file for each note
-     //audio stream it
-     //done
      if (req.slot('Note')) {
-         console.log('incoming note = ' + req.slot('Note'));
          spokenNote = req.slot('Note').toLowerCase().substring(0,1);
-         console.log('extracted note ' + spokenNote);
          note = notes[spokenNote];
      }
      if (!note) {
-         var reprompt = 'What note should we tune.';
-         var prompt = 'I didn\'t hear a note.  Tell me a note.';
+         var reprompt = 'What note should we tune?';
+         var prompt = 'I didn\'t understand which note.  What note should we tune?';
          res.say(prompt).reprompt(reprompt).shouldEndSession(false);
-         //return true;
      } else {
-         res.say('Playing the note ' + note + '.  <audio src="https://dropofapin.com/tune/lowe.mp3" />').send();
-         //return true;
+         var audiourl = 'https://dags.io/tune/' + note + '-note.mp3';
+         res.say('Playing note ' + note + '.  <audio src="' + audiourl + '" />').shouldEndSession(false);
      }
+ }
+);
+
+app.intent('TuneAllIntent', '',
+ function (req, res) {
+     res.say('Let\'s Tune.  <audio src="https://dags.io/tune/all-notes.mp3" /> ' ).send();
+ }
+);
+
+app.intent('AMAZON.HelpIntent', '',
+ function (req, res) {
+     res.say('Ask me to tune all.  To tune a specific note, aske me to tune x.  X can be any of the standard notes on a 6 string guitar.').shouldEndSession(false);
+ }
+);
+
+app.intent('AMAZON.StopIntent', '',
+ function (req, res) {
+     res.say('Goodbye.').send();
  }
 );
 
 app.post = function(req, res, type, exception) {
     if (exception) {
-        return res.clear().say("Oops, something went wrong: " + exception).send();
+        return res.clear().say("Oops, something went wrong.  Please try again.").send();
     }
 };
 
