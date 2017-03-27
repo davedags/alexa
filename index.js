5e1b03846cecdd104a05c75342b372200819b6bc
David D'Agostino <dagostin@symplicity.com>
2017-03-27 13:19:18 -0400

    change node port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var appServer = function(config) {
     log: true,
     debug: false,
     verify: true,
-    port: process.env.port || 8080,
+    port: process.env.port || 1925,
     httpEnabled: true,
     httpsEnabled: false,
     public_html: 'public_html',
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var AlexaAppServer = require("./index.js");
 
 AlexaAppServer.start({
   server_root: './',
-  port: 8080,
+  port: 1925,
   // Use preRequest to load user data on each request and add it to the request json.
   // In reality, this data would come from a db or files, etc.
   preRequest: function(json, req, res) {
